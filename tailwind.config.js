d7d70e38dc6d38fe5ad9a60beb5ebd277873d950
tanyongjia <897795208@qq.com>
2023-01-13 17:14:16 +0800

    fix: first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  purge: {
-    content: [
-      // Use *.tsx if using TypeScript
-      "./pages/**/*.js",
-      "./pages/**/*.ts",
-      "./components/**/*.js",
-      "./components/**/*.ts",
-    ],
-  },
+  content: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.js",
+    "./pages/**/*.ts",
+    "./components/**/*.js",
+    "./components/**/*.ts",
+  ],
 };
