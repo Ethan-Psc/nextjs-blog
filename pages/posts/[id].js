734f35fe2628649b2d661f259d175904f3f7cc03
tanyongjia <897795208@qq.com>
2023-01-13 17:37:11 +0800

    fix: first commit

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from "../../components/layout";
-import Head from "next/Head";
+import Head from "next/head";
 import { getAllPostIds, getPostsData } from "../../lib/posts";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
