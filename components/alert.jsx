776ae059d60e467f82df54c5f5f5ef5220567d87
tanyongjia <897795208@qq.com>
2023-01-13 16:57:53 +0800

    fix: first commit

--- components/alert.jsx
+++ components/alert.jsx
@@ -3,10 +3,8 @@ import cn from 'classnames'
 
 export default function Alert({ children, type }) {
   return (
-    <div
-      className={type === 'success' ? styles.success ? styles.error}
-    >
+    <div className={type === "success" ? styles.success : styles.error}>
       {children}
     </div>
-  )
+  );
 }
